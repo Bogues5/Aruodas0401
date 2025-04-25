6748f563509f8822b23004c7b8d11dafca330062
Bogues5 <suverenas@tutanota.com>
2025-04-25 14:21:05 +0300

    Dabar priskirti kiekvienam string komanda.

--- src/main/java/org/example/models/Plot.java
+++ src/main/java/org/example/models/Plot.java
@@ -1,71 +1,84 @@
 package org.example.models;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
+// Pagal string ištrauktas elementas, kuris parodo kur yra laukelis
 public class Plot {
     public WebDriver driver;
     public String region;
     public String district;
     public String quartal;
     public String street;
-    public String FHouseNum;
+    public String fHouseNum;
     public String show_house_num;
-    public String RCNumber;
-    public String show_rc_number;
-    public String FAreaOverAll;
-
-    public String[] paskirtys = {
-            "Namų valda",
-            "Sklypas soduose",
-            "Sandėliavimo",
-            "Kita",
-            "Daugiabučių statyba",
-            "Miškų ūkio",
-            "Komercinė",
-            "Žemės ūkio",
-            "Pramonės",
-            "Rekreacinė"
-    };
-
-
-    public String[] ypatybes = {
-            "Elektra",
-            "Kraštinis sklypas",
-            "Geodeziniai matavimai",
-            "Dujos",
-            "Greta miško",
-            "Su pakrante",
-            "Vanduo",
-            "Be pastatų",
-            "Asfaltuotas privažiavimas"};
-
-
-
+    public String rcNumber;
+    public String showRcNumber;
+    public String fAreaOverAll;
+    public String[] paskirtys;
+    public String[] ypatybes;
     public String interestedChange;
-    public String notes_lt;
-    public String uploadPhotoBtn;}
+    public String notesLt;
+    public String uploadPhoto;
     public String video;
-    public String tour_3d;
+    public String tour3d;
     public String price;
     public String phone;
     public String email;
-    public String cbdont_show_in_ads;
-    public String dont_want_chat;
+    public String cbDontShowInAds;
+    public String dontWantChat;
+    // Taip pat su masyvu ištraukti elementai su atributu: data-title, kad pasirinkti varianta
+    public String[] datatitle;
+    // su string ištrauktas elementas pagal name , kad pazymetu check box ir paspaustu migtuka
+    public String agreeToRules;
+    public String submitFormButton;
 
-public String[] datatitle = {
-        "Privatus asmuo",
-        "Tarpininkas",
-        "Vystytojas/statytojas",
-        "Kitas verslo subjektas",};
+    public Plot() {
+    }
 
-     public String agree_to_rules;
-     public String submitFormButton;
+    public void fill() {
+        Select savivaldybe = new Select(driver.findElement(By.name("region")));
+        savivaldybe.selectByIndex(1);
+
+        Select gyvenviete = new Select(driver.findElement(By.name("district")));
+        gyvenviete.selectByIndex(1);
+
+        Select mikrorajonas = new Select(driver.findElement(By.name("quartal")));
+        mikrorajonas.selectByIndex(1);
+
+        Select gatve = new Select(driver.findElement(By.name("street")));
+        gatve.selectByIndex(1);
+
+        driver.findElement(By.name("fHouseNum")).sendKeys(this.fHouseNum);
+        driver.findElement(By.name("rcNumber")).sendKeys(this.rcNumber);
+        driver.findElement(By.name("fAreaOverAll")).sendKeys("100");
 
 
+        driver.findElement(By.xpath("//label[@for='cb_FIntendance_property']")).click();
+
+        for (int i = 0; i < this.paskirtys.length; i++) {
+            WebElement vienaPaskirtis = driver.findElement(By.xpath("//input[@data-title='" + this.paskirtys[i] + "']/following-sibling::label"));
+            vienaPaskirtis.click();
+        }
+        for (int i = 0; i < this.ypatybes.length; i++) {
+            WebElement vienaYpatybe = driver.findElement(By.xpath("//input[@data-title='" + this.ypatybes[i] + "']/following-sibling::label"));
+            vienaYpatybe.click();
+        }
+
+        driver.findElement(By.name("notes_lt")).sendKeys("Parduodamas sklypas su elektra.");
+        driver.findElement(By.name("video")).sendKeys("https://youtube.com/example");
+        driver.findElement(By.name("tour_3d")).sendKeys("https://3dturas.example.com");
+        driver.findElement(By.name("price")).sendKeys("50000");
+        driver.findElement(By.name("phone")).sendKeys("+37012345678");
+        driver.findElement(By.name("email")).sendKeys("test@example.com");
 
 
+        driver.findElement(By.xpath("//input[@data-title='Privatus asmuo']")).click();
 
-
+        driver.findElement(By.name("agree_to_rules")).click();
+        driver.findElement(By.name("submitFormButton")).click();
+    }
+}
 
--- src/test/java/PlotTests.java
+++ src/test/java/PlotTests.java
@@ -1,15 +1,105 @@
+
+import org.example.models.Plot;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.time.Duration;
 
 public class PlotTests {
-    public static WebDriver driver;}
+    public static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
 
 
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.aruodas.lt/ideti-skelbima/?obj=11&offer_type=1");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[8]/div[2]/div/div[1]/div/div[2]/div/button[1]")));
+    }
 
+    @Test
+    public void testFillPlotForm() {
+        Plot plot = new Plot();
+        plot.driver = driver;
+        plot.fHouseNum = "123";
+        plot.rcNumber = "1234-5678-9012";
+        plot.paskirtys = new String[]{"Namų valda",""};
+        plot.ypatybes= new String[]{"Elektra", "Vanduo","","","","",""};
+        plot.notesLt = "Parduodamas sklypas su elektra.";
+        plot.video = "https://youtube.com/example";
+        plot.tour3d = "https://3dturas.example.com";
+        plot.price = "50000";
+        plot.phone = "+37012345678";
+        plot.email = "test@example.com";
 
+        // Iškviečiame fill() metodą
+        plot.fill();
 
+//        Select savivaldybe = new Select(driver.findElement(By.name("region")));
+//        Assert.assertEquals(savivaldybe.getFirstSelectedOption().getText(), "Vilniaus m. sav.", "Savivaldybė pasirinkta neteisingai");
+//
+//        Select gyvenviete = new Select(driver.findElement(By.name("district")));
+//        Assert.assertEquals(gyvenviete.getFirstSelectedOption().getText(), "Vilnius", "Gyvenvietė pasirinkta neteisingai");
+//
+//        Select mikrorajonas = new Select(driver.findElement(By.name("quartal")));
+//        Assert.assertEquals(mikrorajonas.getFirstSelectedOption().getText(), "Antakalnis", "Mikrorajonas pasirinktas neteisingai");
+//
+//        Select gatve = new Select(driver.findElement(By.name("street")));
+//        Assert.assertEquals(gatve.getFirstSelectedOption().getText(), "Antakalnio g.", "Gatvė pasirinkta neteisingai");
+//
+//        String actualHouseNum = driver.findElement(By.name("fHouseNum")).getAttribute("value");
+//        Assert.assertEquals(actualHouseNum, "123", "Namo numeris užpildytas neteisingai");
+//
+//        String actualRcNumber = driver.findElement(By.name("rcNumber")).getAttribute("value");
+//        Assert.assertEquals(actualRcNumber, "1234-5678-9012", "RC numeris užpildytas neteisingai");
+//
+//        String actualArea = driver.findElement(By.name("fAreaOverAll")).getAttribute("value");
+//        Assert.assertEquals(actualArea, "100", "Plotas užpildytas neteisingai");
+//
+//        String actualNotes = driver.findElement(By.name("notes_lt")).getAttribute("value");
+//        Assert.assertEquals(actualNotes, "Parduodamas sklypas su elektra.", "Pastabos užpildytos neteisingai");
+//
+//        String actualVideo = driver.findElement(By.name("video")).getAttribute("value");
+//        Assert.assertEquals(actualVideo, "https://youtube.com/example", "Video nuoroda užpildyta neteisingai");
+//
+//        String actualTour3d = driver.findElement(By.name("tour_3d")).getAttribute("value");
+//        Assert.assertEquals(actualTour3d, "https://3dturas.example.com", "3D turo nuoroda užpildyta neteisingai");
+//
+//        String actualPrice = driver.findElement(By.name("price")).getAttribute("value");
+//        Assert.assertEquals(actualPrice, "50000", "Kaina užpildyta neteisingai");
+//
+//        String actualPhone = driver.findElement(By.name("phone")).getAttribute("value");
+//        Assert.assertEquals(actualPhone, "+37012345678", "Telefono numeris užpildytas neteisingai");
+//
+//        String actualEmail = driver.findElement(By.name("email")).getAttribute("value");
+//        Assert.assertEquals(actualEmail, "test@example.com", "El. paštas užpildytas neteisingai");
+//
+//        boolean isIntendanceChecked = driver.findElement(By.id("cb_FIntendance_property")).isSelected();
+//        Assert.assertTrue(isIntendanceChecked, "Checkbox 'cb_FIntendance_property' nepažymėtas");
+//
+//        for (String ypatybe : plot.ypatybes) {
+//            boolean isYpatybeChecked = driver.findElement(By.xpath("//input[@data-title='" + ypatybe + "']")).isSelected();
+//            Assert.assertTrue(isYpatybeChecked, "Ypatybė '" + ypatybe + "' nepažymėta");
+//        }
+//
+//        boolean isPrivatePersonChecked = driver.findElement(By.xpath("//input[@data-title='Privatus asmuo']")).isSelected();
+//        Assert.assertTrue(isPrivatePersonChecked, "Checkbox 'Privatus asmuo' nepažymėtas");
+//
+//        boolean isRulesAgreed = driver.findElement(By.name("agree_to_rules")).isSelected();
+//        Assert.assertTrue(isRulesAgreed, "Checkbox 'agree_to_rules' nepažymėtas");
+    }
 
+    @AfterClass
+    public static void tearDown() {
 
-
-
-
+        }
+    }
